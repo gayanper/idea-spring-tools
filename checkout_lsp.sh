7e1cd52b26c44b70680ffab4c2cbfb2addc7c30e
Gayan Perera <gayanper@gmail.com>
2022-09-26 16:13:08 +0200

    fix script permissions

--- checkout_lsp.sh
+++ checkout_lsp.sh
@@ -3,6 +3,7 @@ cd dependency-build
 
 if [ ! -d lsp4intellij ]; then
   git clone https://github.com/gayanper/lsp4intellij.git && git -C ./lsp4intellij switch -c build-branch remotes/origin/build-branch
+  echo "::info:: Cloned repository [lsp4intellij]"
 fi
 
 cd ../
